a2b5b18e4832955c8b95090542efd477698e05dc
Carlos Montoro Navas <Carlosmon2004@gmail.com>
2023-03-21 22:28:21 +0100

    Creación de la Clase Animal_CarlosMN

---
+++ PracticaCV2_CarlosMN/Animal_CarlosMN.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaCV2_CarlosMN
+{
+    internal class Animal_CarlosMN
+    {
+    }
+}
